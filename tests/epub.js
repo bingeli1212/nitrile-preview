84c78b2c1c98fcadc0504ff957001d1b9d93165e
James Yu <jamesping@yahoo.com>
2020-10-08 15:59:00 -0400

    Updated for the latest epub modes to comply with the latest this.refmap changes

--- tests/epub.js
+++ tests/epub.js
@@ -2,9 +2,6 @@ const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
 const { NitrilePreviewEpub } = require('../lib/nitrile-preview-epub');
 const utils = require('../lib/nitrile-preview-utils');
 
-console.log(process.argv);
-const fname = process.argv[2];
-
 var work = async ()=>{
   console.log(fname);
   var out = await utils.read_file_async(fname);
@@ -13,6 +10,17 @@ var work = async ()=>{
   const translator = new NitrilePreviewEpub(parser);
   parser.read_md_lines(lines);
   const data = await translator.to_epub_document_async();
+  return(data);
 };
 
-work();
+console.log(process.argv);
+const fname = process.argv[2];
+console.log(fname);
+
+if (fname) {
+  work().then(x => console.log(x));
+  setTimeout(function () { }, 1000);
+}else{
+  console.log("Empty file name");
+}
+
