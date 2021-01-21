3c524f0f7f76311656ded8697f21c6f5b64b01b8
James Yu <jamesping@yahoo.com>
2021-01-20 22:39:07 -0500

    Updated

--- tests/slide.js
+++ tests/slide.js
@@ -1,8 +1,7 @@
 const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
 const { NitrilePreviewSlide } = require('../lib/nitrile-preview-slide');
-const utils = require('../lib/nitrile-preview-utils');
 
-var work = async (fname)=>{
+var work = async ()=>{
   const parser = new NitrilePreviewParser();
   const translator = new NitrilePreviewSlide(parser);
   await parser.read_file_async(fname);
@@ -10,12 +9,12 @@ var work = async (fname)=>{
   return(data);
 };
 
-console.log('process.arg=', process.argv);
+console.log(process.argv);
 const fname = process.argv[2];
-console.log('fname=', fname);
+console.log(fname);
 
 if(fname){
-  work(fname).then(data => console.log(data));
+  work().then(x => console.log(x));
   setTimeout(function(){},1000);
 } else {
   console.log("empty file name")
