a0b7c465182f05aee63f37515da453b8309e5ad1
James Yu <jamesping@yahoo.com>
2021-04-09 01:12:17 -0400

    Updated

---
+++ tests/camer.js
@@ -0,0 +1,23 @@
+const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
+const { NitrilePreviewCamer } = require('../lib/nitrile-preview-camer');
+
+var work = async (fname)=>{
+  const parser = new NitrilePreviewParser();
+  await parser.read_file_async(fname);
+  await parser.read_import_async();
+  const translator = new NitrilePreviewCamer(parser);
+  var tex = translator.to_document();
+  return tex;
+};
+
+console.log('process.arg=', process.argv);
+const fname = process.argv[2];
+console.log('fname=', fname);
+
+if(fname){
+  work(fname).then(data => console.log(data));
+  setTimeout(function(){},1000);
+} else {
+  console.log("empty file name")
+}
+
