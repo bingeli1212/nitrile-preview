dc05b06227bfd3a01c6da8cc18d12f6bd8379870
James Yu <jamesping@yahoo.com>
2021-08-04 15:09:14 -0400

    Updated to add a new flowerpot.js file

---
+++ bin/niflowerpot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $1
+#node -e "console.log($1)"
+/usr/local/bin/node -e "const { NitrilePreviewNode } = require ('$HOME/github/nitrile-preview/lib/nitrile-preview-node'); \
+    var node = new NitrilePreviewNode(); \
+    node.to_flowerpot_doc_async('$1').then(([data,xhtmlfile])=>{
+        console.log(xhtmlfile); \
+        node.write_txt_file(xhtmlfile, data).then((xhtmlfile) => { \
+        }); \
+    }) \
+    "
+
+
