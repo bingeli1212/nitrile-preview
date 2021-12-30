564b26fcc3cd0c523a2b0b0826833c8cb06e0b71
James Yu <jamesping@yahoo.com>
2021-12-30 18:57:14 -0500

    Added memoir test target

---
+++ tests/memoir.js
@@ -0,0 +1,25 @@
+const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
+const { NitrilePreviewMemoir } = require('../lib/nitrile-preview-memoir');
+const utils = require('../lib/nitrile-preview-utils');
+
+var work = async ()=>{
+  const parser = new NitrilePreviewParser();
+  const translator = new NitrilePreviewMemoir(parser,program);
+  await parser.read_file_async(fname);
+  
+  var data = translator.to_document();
+  return(data);
+};
+
+console.log(process.argv);
+const fname = process.argv[2];
+const program = 'pdflatex';
+console.log(fname);
+
+if (fname) {
+  work().then(x => console.log(x));
+  setTimeout(function () { }, 1000);
+}else{
+  console.log("Empty file name");
+}
+
