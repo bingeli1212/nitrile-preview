f7adbeed60961bc0439699d4e26f1923d2d9c542
James Yu <jamesping@yahoo.com>
2022-01-04 09:48:57 -0500

    Update folio.js

--- tests/folio.js
+++ tests/folio.js
@@ -5,6 +5,7 @@ var work = async ()=>{
   const parser = new NitrilePreviewParser();
   const translator = new NitrilePreviewFolio(parser);
   await parser.read_file_async(fname);
+  await parser.read_import_async();
   var data = translator.to_document();
   return(data);
 };
